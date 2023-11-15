7ff3a29dd4b94bdc15a31c10911cd55c744e23fb
thuhale2210 <thuhale2210@gmail.com>
2023-11-15 01:14:57 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          TrainTrack
-        </p>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
