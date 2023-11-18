6639ba42653453a0ec6d96b5d207e67df0e306d5
thuhale2210 <thuhale2210@gmail.com>
2023-11-18 18:55:20 -0500

    Add path

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/login';
 import CustomerHome from './components/customerHome';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -9,10 +10,16 @@ function App() {
       <header className="App-header">
         <Router>
           <Routes>
+            {/* Don't forget to change the element to the correct component */}
             <Route path='/' element={<Login />} />
             <Route path='/home' element={<CustomerHome />} />
+            <Route path='/fitness-tracker' element={<CustomerHome />} />
+            <Route path='/exercises' element={<CustomerHome />} />
+            <Route path='/appointment' element={<CustomerHome />} />
+            <Route path='/profile' element={<CustomerHome />} />
           </Routes>
         </Router>
+        <Footer />
       </header>
     </div>
   );
