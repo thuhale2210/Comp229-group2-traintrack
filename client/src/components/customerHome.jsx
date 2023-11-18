577eed9b465f19c972ba10169d7932c8b0e19d17
thuhale2210 <thuhale2210@gmail.com>
2023-11-18 18:54:58 -0500

    Update page layout

--- client/src/components/customerHome.jsx
+++ client/src/components/customerHome.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import NavBar from './navbar'
+import BookAppointment from './bookappointment'
 
 const CustomerHome = () => {
   return (
-    <div className='text-sm text-red-300'>This is customer home</div>
+    <>
+      <NavBar />
+      {/* These divs are temporary. Gonna be replaced with the actual components. */}
+      <div className='h-screen'>
+        <div className='bg-slate-200 w-screen h-1/2 mt-20'>
+          Charts from Fitness Tracker
+        </div>
+        <div className='w-screen h-1/3 flex mt-5'>
+          <div className='w-3/4 bg-slate-200'>
+            Upcoming Appointments
+          </div>
+          <div className='w-1/4'>
+            <BookAppointment className='fixed bottom-0'/>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
