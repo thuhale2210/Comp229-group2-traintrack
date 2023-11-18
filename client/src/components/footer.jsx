88daf568cb7ba0a9c8f54800fa7b5a5be339ca1d
thuhale2210 <thuhale2210@gmail.com>
2023-11-17 20:27:37 -0500

    First commit

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='fixed bottom-0 left-0 right-0 text-center py-2 text-xs'>
+            <div>&copy; 2023 A Product of Aces</div>
+        </div>
+
+    )
+}
+
+export default Footer
