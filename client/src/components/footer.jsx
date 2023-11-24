29d14f8f5f66bd9da8c89b1d2a75a691dea639d9
thuhale2210 <thuhale2210@gmail.com>
2023-11-24 00:26:43 -0500

    Update footer.jsx

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='fixed bottom-0 left-0 right-0 text-center py-2 text-xs'>
+        <div className='fixed bottom-5 left-0 right-0 text-center text-xs'>
             <div>&copy; 2023 A Product of Aces</div>
         </div>
 
