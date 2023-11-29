b655d07063f4bc7a68417230426a41679c5f9e9a
thuhale2210 <thuhale2210@gmail.com>
2023-11-28 22:50:17 -0500

    Update NavBar

--- client/src/components/customerNavBar.jsx
+++ client/src/components/customerNavBar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import Logo from '../images/logo.png'
-import TempAvatar from '../images/temp_avatar.png'
+import Logo from '../images/logo.png';
+import TempAvatar from '../images/temp_avatar.png';
 
 const NavBar = () => {
   const navLinks = [
-    { href: "/home", label: "Home" },
-    { href: "/fitness-tracker", label: "Fitness Tracker" },
-    { href: "/exercises", label: "Exercises" },
-    { href: "/appointment", label: "Appointment" },
-    { href: "/profile", label: "Profile" },
+    { href: '/home', label: 'HOME' },
+    { href: '/fitness-tracker', label: 'FITNESS TRACKER' },
+    { href: '/exercises', label: 'EXERCISES' },
+    { href: '/appointment', label: 'APPOINTMENT' },
+    { href: '/profile', label: 'PROFILE' },
   ];
 
   const location = useLocation();
 
+  const isActive = (match, location) => {
+    if (!match) {
+      return false;
+    }
+    // Check if the current location pathname starts with the link's href
+    return location.pathname.startsWith(match.url);
+  };
+
   return (
     <>
-      <div className='fixed top-0 flex items-center w-full'>
-        <img src={Logo} alt='logo' className='mt-5 ml-7 w-[200px] col-auto' />
-        <nav className='flex flex-row items-center ml-auto'>
+      <div className="fixed top-0 flex items-center w-full">
+        <img src={Logo} alt="logo" className="mt-5 ml-7 w-[200px] col-auto" />
+        <nav className="flex flex-row items-center ml-auto">
           <ul className="mr-2 mt-3 flex flex-row">
-            <div className='text-[17px] p-2 align-middle'>
+            <div className="text-sm p-2 align-middle">
               {navLinks.map((item) => (
                 <NavLink
                   to={item.href}
                   key={item.label}
-                  className={`leading-normal text-slate-gray mx-10 my-1 align-middle ${location.pathname === item.href ? 'font-bold' : ''
+                  isActive={(match, location) => isActive(match, location)}
+                  className={`leading-normal text-slate-gray mx-8 my-1 align-middle ${location.pathname.startsWith(item.href) ? 'font-bold border-primary-gray p-2' : ''
                     }`}
                 >
                   {item.label}
@@ -33,12 +42,12 @@ const NavBar = () => {
               ))}
             </div>
             {/* This is a temporary avatar */}
-            <li><img src={TempAvatar} alt='tempAvatar' className='w-[35px] mx-7 my-1' /></li>
+            <li>
+              <img src={TempAvatar} alt="tempAvatar" className="w-[35px] mx-7 my-1" />
+            </li>
           </ul>
         </nav>
       </div>
-
-
     </>
   );
 };
